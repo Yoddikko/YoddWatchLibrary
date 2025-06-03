2575f10bea9a2135c8adc197b23dcc6def4ec0a1
Alessio I. <alessioiodiceuni@gmail.com>
2025-06-04 00:09:48 +0200

    Initial commit

---
+++ YoddWatchLibrary/YoddWatchLibrary.h
@@ -0,0 +1,18 @@
+//
+//  YoddWatchLibrary.h
+//  YoddWatchLibrary
+//
+//  Created by Ale on 04/06/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YoddWatchLibrary.
+FOUNDATION_EXPORT double YoddWatchLibraryVersionNumber;
+
+//! Project version string for YoddWatchLibrary.
+FOUNDATION_EXPORT const unsigned char YoddWatchLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YoddWatchLibrary/PublicHeader.h>
+
+
